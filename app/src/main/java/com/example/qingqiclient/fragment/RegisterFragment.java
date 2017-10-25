165bf17ca4783ef445d6f1fa04c03cdc897838af
Tony <2271366490@qq.com>
2017-10-25 16:02:08 +0800

    把验证码的第三方短信服务商从Mob换为腾讯云，但是Mob的那些插件和依赖什么的还没有去掉

--- app/src/main/java/com/example/qingqiclient/fragment/RegisterFragment.java
+++ app/src/main/java/com/example/qingqiclient/fragment/RegisterFragment.java
@@ -23,6 +23,7 @@ import com.example.qingqiclient.All_EI_Info;
 import com.example.qingqiclient.R;
 import com.example.qingqiclient.utils.CheckInputUtils;
 import com.example.qingqiclient.utils.Constant;
+import com.example.qingqiclient.utils.CountDownTimerUtils;
 import com.mob.MobSDK;
 
 import org.json.JSONException;
@@ -46,6 +47,7 @@ import static cn.smssdk.SMSSDK.getVerificationCode;
 /**
  *这个碎片主要用来进行用户的注册
  * 注意权限是批量申请的
+ * 一开始是使用Mob，现在改用腾讯云 2017/10/25
  */
 public class RegisterFragment extends Fragment implements View.OnClickListener{
 
@@ -55,16 +57,12 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     private EditText usertel;   //用户输入手机号注册
     private EditText password;  //用户输入密码
     private EditText password_confirm;  //用户再次输入密码确认
-    //全局变量Handler，处理验证码回调
-    private EventHandler handler;
 
-    //要申请的权限
-    private static final String[] PERMISSION_REQUEST_LIST = {
-            Manifest.permission.READ_CONTACTS,  Manifest.permission.READ_PHONE_STATE,  Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.RECEIVE_SMS,    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.GET_TASKS,
-            Manifest.permission.READ_SMS, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.INTERNET
-    };
+    //验证码全局变量
+    private static String code = new String("0");
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -77,63 +75,14 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
         usertel = (EditText) v.findViewById(R.id.register_tel);
         password = (EditText) v.findViewById(R.id.register_password);
         password_confirm = (EditText) v.findViewById(R.id.register_password_confirm);
+        //提交按钮要先设置为不可见
+        submmit.setVisibility(View.INVISIBLE);
 
 
         send_checkCode_btn.setOnClickListener(this);
         submmit.setOnClickListener(this);
 
-        //在onCreate的方法里对SDK进行初始化
-        MobSDK.init(getContext(), "21bdeae252f01","27d63bf79a9c63d7839de041f8603422");
-        handler = new EventHandler(){
-            @Override
-            public void afterEvent(int event, int result, Object data) {
-                if (result == SMSSDK.RESULT_COMPLETE){
-                    //回调完成
-                    if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
-                        //提交验证码成功
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getContext(),"验证成功",Toast.LENGTH_SHORT).show();
-                                //下面执行验证码验证成功之后的逻辑，实行注册
-                                sendRegisterWithOkHttp(Constant.getServer() + "/user/register?tel=" + usertel.getText().toString() +
-                                "&password=" + password.getText().toString());
-                            }
-                        });
 
-                    }else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE){
-                        //获取验证码成功
-                        getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getContext(),"验证码已发送",Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }else if (event ==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
-                    }
-                }else{
-                    ((Throwable)data).printStackTrace();
-                    Throwable throwable = (Throwable) data;
-                    try {
-                        JSONObject obj = new JSONObject(throwable.getMessage());
-                        final String des = obj.optString("detail");
-                        if (!TextUtils.isEmpty(des)){
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(getContext(),"提交错误信息",Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-        };
-
-        SMSSDK.registerEventHandler(handler);
 
         //要先对加密开源库进行初始化
         try {
@@ -194,77 +143,45 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     }
 
 
-    public void play(View view, String tel) {
-        //获取验证码
-        SMSSDK.getVerificationCode("86",tel);
-    }
-
-    public void tijiao(View view, String tel) {
-        //在提交按钮的监听事件里先通过EditText获取到用户所输入的验证码，然后和刚才的那个手机号一起提交到后台验证
-        String number = checkCode.getText().toString();
-        SMSSDK.submitVerificationCode("86",tel,number);
-        //反注册将Handler销毁
-        SMSSDK.unregisterEventHandler(handler);
-    }
-
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.send_checkCode:
-                //先申请一下各种运行时权限
-                //先检查还有哪些运行时权限没有申请
-                String[] new_Request_Permission_List = checkPermission();
-                if (new_Request_Permission_List.length != 0){
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        ActivityCompat.requestPermissions(getActivity(), new_Request_Permission_List, 1);
-                    }
-                }
-
-                //判断用户手机号合法不合法
-                String tel = usertel.getText().toString();
-                Log.e("Tel", tel);
-                if (CheckInputUtils.checkTel(tel)){
-                    play(view, tel);
-                } else {
-                    Toast.makeText(getContext(), "手机号输入不合法，请输入注入:18895621356的形式", Toast.LENGTH_SHORT).show();
-                }
-
+                //执行发送验证码的逻辑，我们放到一个向其情服务器的网络请求中，请求到的验证码赋值到code中
+                sendRequestForCode(usertel.getText().toString());
                 break;
             case R.id.submmit:
-                //检测两次输入的密码是否合法且相同
-                if (CheckInputUtils.checkPassword(password.getText().toString()) && CheckInputUtils.checkPassword(password_confirm.getText().toString())){
-                    if (password.getText().toString().equals(password_confirm.getText().toString())){
-                        //判断用户手机号合法不合法，要注意这里也要对手机号进行验证，因为可能会出现用户在申请验证码之后更改手机号的状况
-                        String tel_submmit = usertel.getText().toString();
-                        if (CheckInputUtils.checkTel(tel_submmit)){
-                            //我们把网络请求的方法写在提交之中
-                            tijiao(view,tel_submmit);
-                        }
-                    }else {
-                        Toast.makeText(getContext(), "对不起，请再次确认密码！", Toast.LENGTH_SHORT).show();
-                    }
-                }else {
-                    Toast.makeText(getContext(), "对不起，密码格式错误!", Toast.LENGTH_SHORT).show();
+                //执行验证注册逻辑
+                //先判断手机号是否合法
+                if (!CheckInputUtils.checkTel(usertel.getText().toString())){
+                    //如果不合法，则提示
+                    Toast.makeText(getContext(), "手机号不合法，输入的手机号不能带空格", Toast.LENGTH_SHORT).show();
+                    break;
                 }
-
-                break;
-        }
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch(requestCode){
-            case 1:
-                if (grantResults != null && permissions != null) {
-                    for (int i = 0; i < grantResults.length; i++) {
-                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
-                            Toast.makeText(getContext(), "抱歉，您没有同意足够的权限，系统可能无法为您正常服务！", Toast.LENGTH_SHORT).show();
-                            break;
-                        }
-                    }
+                Boolean flag = CheckInputUtils.checkPassword(password.getText().toString()) & CheckInputUtils.checkPassword(password_confirm.getText().toString()); //手机号合法则为TRUE
+                if (!flag){
+                    Toast.makeText(getContext(), "密码必须是由6到26位的由字母、数字、下划线组成的字符串", Toast.LENGTH_SHORT).show();
+                    break;
                 }
-
+                if (!password.getText().toString().equals(password_confirm.getText().toString())){
+                    Toast.makeText(getContext(), "新老密码不相同", Toast.LENGTH_SHORT).show();
+                    password.setText("");
+                    password_confirm.setText("");
+                    break;
+                }
+                if (code == null){
+                    Toast.makeText(getContext(), "验证码错误", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                if (!code.equals(checkCode.getText().toString())){
+                    Toast.makeText(getContext(), "验证码输入错误", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                //走到这一步，就可以向服务器发送注册请求了
+                //先生成请求路径
+                String url = Constant.getServer() + "/user/register?tel=" + usertel.getText().toString() +
+                        "&password=" + password.getText().toString();
+                sendRegisterWithOkHttp(url);
                 break;
             default:
                 break;
@@ -272,21 +189,49 @@ public class RegisterFragment extends Fragment implements View.OnClickListener{
     }
 
     /**
-     * 根据申请列表组建未申请的权限列表
+     * 这个方法用于向我们的服务器请求验证码
+     * @param tel
+     * @return
      */
-    private String[]  checkPermission() {
+    private void sendRequestForCode(final String tel) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    //发送请求，获得验证码
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            //指定访问的远程服务器
+                            .url(Constant.getServer() + "/sms/getCode?tel=" + tel).build();
+                    Response response = client.newCall(request).execute();
+                    code = response.body().string();
+                    response.body().close();
 
-        ArrayList<String> stringsList = new ArrayList<>();
-        for (String permission : PERMISSION_REQUEST_LIST){
-            if (ContextCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED){
-                //如果权限已经申请，那么就不申请
-                stringsList.add(permission);
+                    if (!code.equals("0")){
+                        //发送验证码成功
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //执行计时逻辑，此处参考博客（http://blog.csdn.net/shineflowers/article/details/50111777）
+                                CountDownTimerUtils mCountDownTimerUtils = new CountDownTimerUtils(send_checkCode_btn, 60000, 1000);
+                                mCountDownTimerUtils.start();
+                                //把提交按钮设置成可点击
+                                submmit.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }else {
+                        //请求验证码失败逻辑
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getContext(), "请求验证码失败，请检查您的网络", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-
-        String[] strings = new String[stringsList.size()];
-
-
-        return  stringsList.toArray(strings);
+        }).start();
     }
 }
